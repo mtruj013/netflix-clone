87bc7b50bccef1057b03a351e681c7b9dab14e31
Maria Trujillo <mptruj015@gmail.com>
2021-06-01 15:33:55 +0200

    imported global styles

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { GlobalStyles } from './global-styles';
+import 'normalize.css';
 
-
-ReactDOM.render(
-
-    <App />
-,
+ReactDOM.render(<>
+  <GlobalStyles />
+  <App />
+</>,
   document.getElementById('root')
 );
