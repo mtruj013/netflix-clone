c4b0e2db5f93946b3f28c8d7b3b46e13d444f13a
Maria Trujillo <mptruj015@gmail.com>
2021-06-02 19:30:04 +0200

    included opt form in accordion

--- client/src/containers/accordion.js
+++ client/src/containers/accordion.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import faqsData from '../data/faqs.json';
 import Accordion from '../components/accordion';
+import OptForm from '../components/opt-form';
 
 function FaqsContainer () {
     return (
@@ -12,6 +13,13 @@ function FaqsContainer () {
                     <Accordion.Body>{item.body}</Accordion.Body>
                 </Accordion.Item>
             ))}
+
+            <OptForm>
+                <OptForm.Input placeholder="Email address"/>
+                <OptForm.Button>Try it now</OptForm.Button>
+                <OptForm.Break/>
+                <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+            </OptForm>
         </Accordion>
     )
 }
